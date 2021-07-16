ec7178a7778044c09092ef6f3c5ee5d04eb16b9f
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-07-16 14:47:43 +0900

    Create Task Fibonacci

--- MyProject1/SampleGhTaskFibonacciComponent.cs
+++ MyProject1/SampleGhTaskFibonacciComponent.cs
@@ -21,6 +21,7 @@ namespace MyProject1
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddIntegerParameter("Steps", "S", "Number of steps to compute.", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -28,6 +29,32 @@ namespace MyProject1
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddIntegerParameter("Fibonacci number", "F", "The Fibonacci number", GH_ParamAccess.item);
+        }
+        public class SolveResults
+        {
+            public int Value { get; set; }
+        }
+
+        private static SolveResults ComputeFibonacci(int n)
+        {
+            SolveResults result = new SolveResults();
+            if (n == 0)
+                result.Value = 0;
+            else if (n == 1)
+                result.Value = 1;
+            else
+            {
+                int x = 0, y = 1, rc = 0;
+                for (int i = 2; i <= n; i++)
+                {
+                    rc = x + y;
+                    x = y;
+                    y = rc;
+                }
+                result.Value = rc;
+            }
+            return result;
         }
 
         /// <summary>
@@ -36,8 +63,57 @@ namespace MyProject1
         /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-        }
+            const int max_steps = 46;
 
+            if (InPreSolve)
+            {
+                // First pass; collect data and construct tasks
+                int steps = 0;
+                DA.GetData(0, ref steps);
+                if (steps < 0)
+                {
+                    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Steps must be >= 0.");
+                    return;
+                }
+                if (steps > max_steps) // Prevent overflow...
+                {
+                    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"Steps must be <= {max_steps}.");
+                    return;
+                }
+
+                // Run the task
+                Task<SolveResults> task = Task.Run(() => ComputeFibonacci(steps), CancelToken);
+                TaskList.Add(task);
+                return;
+            }
+
+            if (!GetSolveResults(DA, out SolveResults result))
+            {
+                // Compute right here, right now.
+                // 1. Collect
+                int steps = 0;
+                DA.GetData(0, ref steps);
+                if (steps < 0)
+                {
+                    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Steps must be >= 0.");
+                    return;
+                }
+                if (steps > max_steps) // Prevent overflow...
+                {
+                    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"Steps must be <= {max_steps}.");
+                    return;
+                }
+
+                // 2. Compute
+                result = ComputeFibonacci(steps);
+            }
+
+            // 3. Set
+            if (result != null)
+            {
+                DA.SetData(0, result.Value);
+            }
+        }
         /// <summary>
         /// Provides an Icon for the component.
         /// </summary>
