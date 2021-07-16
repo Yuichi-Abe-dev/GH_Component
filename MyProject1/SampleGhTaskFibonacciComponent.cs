467de5ec85c775bc871c34a3c9909fbe2b110b02
Yuichi-Abe-dev <66289694+Yuichi-Abe-dev@users.noreply.github.com>
2021-07-16 13:52:06 +0900

    Create New CS FIle

---
+++ MyProject1/SampleGhTaskFibonacciComponent.cs
@@ -0,0 +1,62 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+
+namespace MyProject1
+{
+    public class SampleGhTaskFibonacciComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the SampleGhTaskFibonacciComponent class.
+        /// </summary>
+        public SampleGhTaskFibonacciComponent()
+          : base("Task Fibonacci", "TFib", "Task computes a Fibonacci number.",
+              "Extra", "Util")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("8d24f94d-ea69-4646-88f2-6d33ce0e185e"); }
+        }
+    }
+}
